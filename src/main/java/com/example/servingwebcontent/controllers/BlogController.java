d4e757bad7ce514bf5a558cd812c7f65cf3c7055
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-06-19 14:40:51 +0300

    0.0.4

--- src/main/java/com/example/servingwebcontent/controllers/BlogController.java
+++ src/main/java/com/example/servingwebcontent/controllers/BlogController.java
@@ -1,15 +1,42 @@
 package com.example.servingwebcontent.controllers;
 
+import com.example.servingwebcontent.models.Post;
+import com.example.servingwebcontent.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blog(Model model){
         model.addAttribute("title", "Страница про нас");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        model.addAttribute("title", "Страница про нас");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text,
+                              Model model){
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
 
