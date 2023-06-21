ef21a40d436994a2d00c6491c7433e4232c95afc
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-06-21 11:48:49 +0300

    0.0.5

--- src/main/java/com/example/servingwebcontent/controllers/BlogController.java
+++ src/main/java/com/example/servingwebcontent/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -23,7 +27,7 @@ public class BlogController {
         return "blog-main";
     }
 
-    @GetMapping("/blog/add")
+    @GetMapping("/blog/add") //Получение из базы
     public String blogAdd(Model model){
         model.addAttribute("title", "Страница про нас");
         Iterable<Post> posts = postRepository.findAll();
@@ -31,11 +35,59 @@ public class BlogController {
         return "blog-add";
     }
 
-    @PostMapping("/blog/add")
+    @PostMapping("/blog/add") // Добавление
     public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text,
                               Model model){
         Post post = new Post(title, anons, full_text);
         postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit") // Получение для редактирования
+    public String blogEdit(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit") // Редактирование
+    public String blogPostUpdate(@PathVariable(value = "id") long id,@RequestParam String title, @RequestParam String anons,
+                                 @RequestParam String full_text, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove") // Удаление
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model){
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+
         return "redirect:/blog";
     }
 }
